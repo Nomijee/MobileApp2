033f26b65c4a9fb0f9cb140f13af0fcb7bc8548d
Nomijee <33327266+Nomijee@users.noreply.github.com>
2019-01-23 15:47:28 +0000

    typescript

--- lab1.js
+++ lab1.js
@@ -1,5 +1,24 @@
-function addition(value) { console.log("Value is: " + value); }
+function addition(value) {
+    console.log("Value is: " + value);
+}
 var firstVal = 42;
 var secondVal = 1;
 var sumOfVals = (firstVal + secondVal).toLocaleString();
 addition(sumOfVals);
+var flag = true;
+console.log("Value Assisgned  to flag is " + flag);
+function str_len(value) { var num = value.length; return num; }
+function str_len_nospaces(value) { var num = value.replace(/\s+/, "").length; return num; }
+console.log("String length with spaces and all is: " + str_len("test 1"));
+console.log("String length with spaces not included in the count: " + str_len_nospaces("test 1"));
+var num;
+if (spaces) {
+    num = value.replace(/\s+/, "").length;
+}
+else {
+    num = value.length;
+}
+return num;
+console.log("Function combined: String length with spaces and all is: " + str_len_both("test 1", false));
+console.log("Function combined: String length with spaces not included in the count: " + str_len_both("test 1", true));
+console.log("Function combined: String length with spaces and all is, not setting spaces parameter so will default to false: " + str_len_both("test 1"));
--- lab1.ts
+++ lab1.ts
@@ -1,3 +1,18 @@
-function addition(value: string) { console.log("Value is: " + value); }
-let firstVal: number = 42; let secondVal: number = 1; let sumOfVals: string = (firstVal + secondVal).toLocaleString();
+function addition(value: string) 
+{ console.log("Value is: " + value); 
+}
+let firstVal: number = 42;
+ let secondVal: number = 1; 
+ let sumOfVals: string = (firstVal + secondVal).toLocaleString();
 addition(sumOfVals); 
+
+let flag:boolean = true;
+console.log("Value Assisgned  to flag is "+flag)
+function str_len(value: string): number{ let num: number = value.length; return num; }
+function str_len_nospaces(value: string): number{ let num: number = value.replace(/\s+/, "").length; return num; }
+console.log("String length with spaces and all is: "+str_len("test 1")); console.log("String length with spaces not included in the count: " +str_len_nospaces("test 1"));
+
+let num: number; if(spaces){ num = value.replace(/\s+/, "").length; }else{ num = value.length; } return num;
+
+console.log("Function combined: String length with spaces and all is: "+str_len_both("test 1",false)); console.log("Function combined: String length with spaces not included in the count: "+str_len_both("test 1", true)); console.log("Function combined: String length with spaces and all is, not setting spaces parameter so will default to false: "+str_len_both("test 1"));
+}
